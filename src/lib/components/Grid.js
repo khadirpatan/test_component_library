8fd42a1f5477725b8b5169713425c62f1ece0ea7
khadirpatan <khadirpatan1@gmail.com>
2022-11-24 16:23:49 +0530

    Grid added

---
+++ src/lib/components/Grid.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const GridCustomized = ({ props }) => {
+  return <Grid {...props}></Grid>;
+};
+
+export default GridCustomized;
